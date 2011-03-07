7128c62be80d97e6722e5c4f065d931d215df574
Drew Dara-Abrams <dda@dara-abrams.com>
2011-03-07 00:18:40 -0800

    forgot to add new db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "activities", :force => true do |t|
     t.integer  "participant_id"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 4) do
   end
 
   add_index "activities", ["participant_id"], :name => "index_activities_on_participant_id"
+
+  create_table "activties_trips", :id => false, :force => true do |t|
+    t.integer "activity_id"
+    t.integer "trip_id"
+  end
 
   create_table "administrators", :force => true do |t|
     t.string   "first_name"
@@ -331,5 +336,24 @@ ActiveRecord::Schema.define(:version => 4) do
   add_index "trips", ["end_travel_fix_id"], :name => "index_trips_on_end_travel_fix_id"
   add_index "trips", ["participant_id"], :name => "index_trips_on_participant_id"
   add_index "trips", ["start_travel_fix_id"], :name => "index_trips_on_start_travel_fix_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
