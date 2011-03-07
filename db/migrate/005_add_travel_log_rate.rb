5868fed2ca284557f0fe132622c708649a61b331
Drew Dara-Abrams <dda@dara-abrams.com>
2011-03-06 22:20:55 -0800

    oops, fix

--- db/migrate/005_add_travel_log_rate.rb
+++ db/migrate/005_add_travel_log_rate.rb
@@ -2,7 +2,6 @@ class AddTravelLogRate < ActiveRecord::Migration
   def self.up
     add_column :devices, :travel_log_rate, :int
     add_column :devices, :travel_log_adapt_rate, :boolean
-    marcc
   end
 
   def self.down
