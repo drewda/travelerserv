a23207e814a3c4f8d2e54cadfc07f07828fc979f
Drew Dara-Abrams <dda@dara-abrams.com>
2011-03-13 21:38:39 -0700

    log more data from Enduro: speed, altitude, accuracy (which is a qualitative measure; see the @TRACK protocol manual p. 13

--- lib/enduro/enduro_server.rb
+++ lib/enduro/enduro_server.rb
@@ -14,12 +14,19 @@ module GeoTransServer
     data = data.split(',')
     if data[0] == '+RESP:GTRTL' or data[0] == '+RESP:GTTRI'
       imei = data[1]
+      speed = data[6] #km/hr
+      #heading = data[7]
+      altitude = data[8] #m
+      accuracy = data[9] # Enduro accuracy is a qualitative measure; see @TRACK protocol manual p. 13
       lon = data[10]
       lat = data[11]
       if lon != 0 and lat != 0 
         RestClient.post 'http://traveler.cocogeo.com/mobile/enduro/travel_fixes', 
                         'travel_fix[longitude]' => lon,
                         'travel_fix[latitude]' => lat,
+                        'travel_fix[speed]' => speed,
+                        'travel_fix[altitude]' => altitude,
+                        'travel_fix[accuracy]' => accuracy,
                         'device[identification]' => imei
         puts 'travel_fix posted'
       end
